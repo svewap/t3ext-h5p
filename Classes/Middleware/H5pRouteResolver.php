1d4daf857252ab51f803fd004d83a6361f504f53
Sven Wappler <info@wappler.systems>
2021-09-10 15:23:31 +0200

    Routing improved

--- Classes/Middleware/H5pRouteResolver.php
+++ Classes/Middleware/H5pRouteResolver.php
@@ -23,9 +23,7 @@ use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 use TYPO3\CMS\Core\Http\HtmlResponse;
 use TYPO3\CMS\Core\Http\RequestFactory;
-use TYPO3\CMS\Core\Http\Response;
 use TYPO3\CMS\Core\LinkHandling\LinkService;
-use TYPO3\CMS\Core\Resource\File;
 use TYPO3\CMS\Core\Routing\InvalidRouteArgumentsException;
 use TYPO3\CMS\Core\Routing\RouterInterface;
 use TYPO3\CMS\Core\Site\Entity\Site;
@@ -62,43 +60,27 @@ class H5pRouteResolver implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        if (($site = $request->getAttribute('site', null)) instanceof Site &&
-            ($configuration = $site->getConfiguration()['routes'] ?? null)
-        ) {
+        if (($site = $request->getAttribute('site', null)) instanceof Site) {
             $path = ltrim($request->getUri()->getPath(), '/');
-            foreach ($configuration as $route) {
-                $routePath = ltrim(trim($site->getBase()->getPath(), '/') . '/' . ltrim($route['route'], '/'), '/');
-                if ($route['type'] === 'h5pembed' && strpos($path, $routePath) !== false) {
 
-                    $source = 't3://page?uid='.$site->getRootPageId().'&type=723442';
-                    $params = explode('/',$path);
-                    $id = (int)$params[array_key_last($params)];
+            if (strpos($path, 'h5p/embed/') !== false) {
+                $source = 't3://page?uid='.$site->getRootPageId().'&type=723442';
+                $params = explode('/',$path);
+                $id = (int)$params[array_key_last($params)];
 
-                    $source .= '&tx_h5p_embedded[contentId]='.$id;
+                $source .= '&tx_h5p_embedded[contentId]='.$id;
 
-                    $urlParams = $this->linkService->resolve($source);
-                    $uri = $urlParams['url'] ?? $this->getPageUri($request, $site, $urlParams);
-                    [$content, $contentType] = $this->getFromUri($uri);
+                $urlParams = $this->linkService->resolve($source);
+                $uri = $urlParams['url'] ?? $this->getPageUri($request, $site, $urlParams);
+                [$content, $contentType] = $this->getFromUri($uri);
 
-                    return new HtmlResponse($content, 200, ['Content-Type' => $contentType]);
-                }
+                return new HtmlResponse($content, 200, ['Content-Type' => $contentType]);
             }
 
         }
         return $handler->handle($request);
     }
 
-
-    /**
-     * @param File $file
-     * @return array
-     */
-    protected function getFromFile(File $file): array
-    {
-        $content = $file->getContents();
-        $contentType = $file->getMimeType();
-        return [$content, $contentType];
-    }
 
     /**
      * @param string $uri
